c91b0701e08ff393ffe923da5bee4568c8e48f96
rachelpatrocinio <rachelannepatrocinio@gmail.com>
2024-05-06 15:40:09 +0200

    Initial Commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+    $hotels = [
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+    ];
+
+    for($i = 0; $i < count($hotels); $i++){
+        $hotel = $hotels[$i];
+        $name = $hotel["name"];
+        $description = $hotel["description"];
+        $parking = $hotel["parking"];
+        $vote = $hotel["vote"];
+        $distance = $hotel["distance_to_center"];
+    }
+?>
