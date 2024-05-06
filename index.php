bf9efe4d179f112b1b509d562e33291168124908
rachelpatrocinio <rachelannepatrocinio@gmail.com>
2024-05-06 17:09:28 +0200

    Added: Bootstrap and printed on table

--- index.php
+++ index.php
@@ -36,23 +36,54 @@
             'distance_to_center' => 50
         ],
     ];
-
-    for($i = 0; $i < count($hotels); $i++){
-        $hotel = $hotels[$i];
-        $name = $hotel["name"];
-        $description = $hotel["description"];
-        $parking = $hotel["parking"];
-        $vote = $hotel["vote"];
-        $distance = $hotel["distance_to_center"];
-
-        ?>
-        <ul>
-            <li> <?php echo $name; ?></li>
-            <li> <?php echo $description; ?></li>
-            <li> <?php echo $parking; ?></li>
-            <li> <?php echo $vote; ?></li>
-            <li> <?php echo $distance; ?></li>
-        </ul>
-        <?php
-    }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>php-hotel</title>
+</head>
+<body>
+
+    <div class="container">
+        <div class="row">
+            <table class="table">
+                <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Parking</th>
+                    <th scope="col">Vote</th>
+                    <th scope="col">Distance</th>
+                </tr>
+
+                <?php
+                for($i = 0; $i < count($hotels); $i++){
+                    $hotel = $hotels[$i];
+                    $name = $hotel["name"];
+                    $description = $hotel["description"];
+                    $parking = $hotel["parking"];
+                    $vote = $hotel["vote"];
+                    $distance = $hotel["distance_to_center"];
+                ?>
+
+                    <tr>
+                        <td><?php echo $name?></td>
+                        <td><?php echo $description?></td>
+                        <td><?php echo $parking?></td>
+                        <td><?php echo $vote?></td>
+                        <td><?php echo $distance?></td>
+                    </tr>
+
+                <?php
+                }
+                ?>
+            </table>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+</body>
+</html>
